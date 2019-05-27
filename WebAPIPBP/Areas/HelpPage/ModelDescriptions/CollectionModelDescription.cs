5972131999817398c23edabf540dd8cb9939d039
Davi Rocha <davirmsousa@hotmail.com>
2019-05-27 17:10:34 -0300

    Adição do projeto de WebAPI
    A API já referencia o projeto WEB para futura utilização dos serviços.

---
+++ WebAPIPBP/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WebAPIPBP.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ WebAPIPBP/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebAPIPBP.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
